81dafb7fd166de66f0563866738f986eec51ff35
XSakibxHossainX <sakibxhossian021@gmail.com>
2023-10-21 12:41:03 +0600

    feat: Updated data.sql

---
+++ 1. REST APIs/lab2/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Cats (
+ id BIGINT,
+ name VARCHAR(255),
+ breed VARCHAR(255),
+ origin VARCHAR(255)
+ );
+
+INSERT INTO Cats (id, name, breed, origin) VALUES (1, 'Fluffy', 'Pomeranian', 'Mountain View, CA');
+INSERT INTO Cats (id, name, breed, origin) VALUES (2, 'Spot', 'Pit Bull', 'Austin, TX');
+INSERT INTO Cats (id, name, breed, origin) VALUES (3, 'Ginger', 'Cocker Spaniel', 'Kansas City, KS');
+INSERT INTO Cats (id, name, breed, origin) VALUES (4, 'Lady', 'Direwolf', 'The North');
+INSERT INTO Cats (id, name, breed, origin) VALUES (5, 'Sasha', 'Husky', 'Buffalo, NY');
