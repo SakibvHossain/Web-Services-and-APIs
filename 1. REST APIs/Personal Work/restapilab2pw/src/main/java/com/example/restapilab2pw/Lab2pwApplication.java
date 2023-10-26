ce67d66bf05b7a9f37815c5a0070622b45a6cd1a
SakibvHossain <sakibxhossian021@gmail.com>
2023-10-26 22:13:14 +0600

    initial: Added my own work to create REST API

---
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/Lab2pwApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapilab2pw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2pwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2pwApplication.class, args);
+	}
+
+}
---
+++ 1. REST APIs/Personal Work/restapilab2pw/src/test/java/com/example/restapilab2pw/Lab2pwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapilab2pw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2pwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
