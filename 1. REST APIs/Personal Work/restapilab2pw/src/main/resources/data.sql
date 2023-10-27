45cc0e144ac065898e52322109c5e18549f68ab2
SakibvHossain <sakibxhossian021@gmail.com>
2023-10-27 21:54:09 +0600

    initial: Added new sql files with sql query

---
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO student (id, name, batch, department, blood) VALUES (1, 'Anika', '14', 'Robotics', 'AB+');
+INSERT INTO student (id, name, batch, department, blood) VALUES (2, 'Aysha', '14', 'Robotics', 'AB+');
+INSERT INTO student (id, name, batch, department, blood) VALUES (3, 'Tanha', '14', 'Robotics', 'AB+');
+INSERT INTO student (id, name, batch, department, blood) VALUES (4, 'Nusaybah', '14', 'Robotics', 'AB+');
+INSERT INTO student (id, name, batch, department, blood) VALUES (5, 'Rabea', '14', 'Robotics', 'AB+');
---
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student(
+id BIGINT,
+name VARCHAR(255),
+batch VARCHAR(255),
+department VARCHAR(255),
+blood VARCHAR(255)
+);
