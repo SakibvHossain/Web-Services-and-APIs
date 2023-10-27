ea5ca7505ac6261fe8c456ff4ab975c387c2ad0e
SakibvHossain <sakibxhossian021@gmail.com>
2023-10-27 23:10:42 +0600

    feat: StudentServerImplementation.java has been documented

--- 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentServerImplementation.java
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentServerImplementation.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+/**
+ * The StudentServerImplementation class is a service implementation that retrieves student data from a
+ * repository.
+ */
 @Service
 public class StudentServerImplementation implements StudentService{
     @Autowired
