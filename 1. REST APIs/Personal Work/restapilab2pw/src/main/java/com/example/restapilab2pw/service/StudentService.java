c57988fa27a868728964ba46bf97ab448b928c2f
SakibvHossain <sakibxhossian021@gmail.com>
2023-10-27 22:51:43 +0600

    feat: Added methods for performing some operations

--- 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentService.java
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentService.java
@@ -1,4 +1,14 @@
 package com.example.restapilab2pw.service;
 
-public class StudentService {
+import com.example.restapilab2pw.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> retrieveAllStudents();
+    List<String> retrieveAllStudentNames();
+    List<String> retrieveAllStudentBatch();
+    List<String> retrieveAllStudentDepartment();
+    List<String> retrieveAllStudentBloods();
+    String retrieveStudentById(Long id);
 }
