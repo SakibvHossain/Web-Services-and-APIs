5a8b25b4bacc64a57be028025d7500ca01fd39b5
SakibvHossain <sakibxhossian021@gmail.com>
2023-10-27 22:50:43 +0600

    feat: Added Spring jpa query

--- 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/repository/StudentRepository.java
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/repository/StudentRepository.java
@@ -1,4 +1,26 @@
 package com.example.restapilab2pw.repository;
 
-public class StudentRepository {
+import com.example.restapilab2pw.entity.Student;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+    @Query("select d.id, d.name, d.department from Student d where id=:id")
+    String findAllNameAlsoDepartment(Long id);
+
+    @Query("select d.id, d.name from Student d")
+    List<String> findAllName();
+
+    @Query("select d.id, d.department from Student d")
+    List<String> findAllDepartment();
+
+    @Query("select d.id, d.batch from Student d")
+    List<String> findAllBatch();
+
+    @Query("select d.id, d.blood from Student d")
+    List<String> findAllBlood();
+
 }
