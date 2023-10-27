7124d558b7e741874c88c7964861f6c5b5203ce5
SakibvHossain <sakibxhossian021@gmail.com>
2023-10-27 22:52:32 +0600

    feat: Implemented StudentService methods

--- 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentServerImplementation.java
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentServerImplementation.java
@@ -1,4 +1,45 @@
 package com.example.restapilab2pw.service;
 
-public class StudentServerImplementation {
+import com.example.restapilab2pw.entity.Student;
+import com.example.restapilab2pw.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServerImplementation implements StudentService{
+    @Autowired
+    StudentRepository repository;
+
+    @Override
+    public List<Student> retrieveAllStudents() {
+        return (List<Student>) repository.findAll();
+    }
+
+    @Override
+    public List<String> retrieveAllStudentNames() {
+        return repository.findAllName();
+    }
+
+    @Override
+    public List<String> retrieveAllStudentBatch() {
+        return repository.findAllBatch();
+    }
+
+    @Override
+    public List<String> retrieveAllStudentDepartment() {
+        return repository.findAllDepartment();
+    }
+
+    @Override
+    public List<String> retrieveAllStudentBloods() {
+        return repository.findAllBlood();
+    }
+
+    //We will take care of it later
+    @Override
+    public String retrieveStudentById(Long id) {
+        return repository.findAllNameAlsoDepartment(id);
+    }
 }
