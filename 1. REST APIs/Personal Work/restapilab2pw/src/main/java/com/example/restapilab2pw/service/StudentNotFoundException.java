ba4f70b6e364d381175b19e667c0a53154dab75a
SakibvHossain <sakibxhossian021@gmail.com>
2023-10-27 23:11:45 +0600

    feat: Extends RuntimeException to handle exception

--- 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentNotFoundException.java
+++ 1. REST APIs/Personal Work/restapilab2pw/src/main/java/com/example/restapilab2pw/service/StudentNotFoundException.java
@@ -1,4 +1,10 @@
 package com.example.restapilab2pw.service;
 
-public class StudentNotFoundException {
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException() {
+    }
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
 }
